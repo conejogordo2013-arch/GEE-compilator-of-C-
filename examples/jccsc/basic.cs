64570c328eefcc5ae10d736e274367a8210935a1
conejogordo2013-arch <conejogordo2013@gmail.com>
2026-04-14 19:47:00 -0600

    Add incremental cache pipeline and IR optimization to JCCSC

---
+++ examples/jccsc/basic.cs
@@ -0,0 +1,42 @@
+global using Core = System.Collections;
+using IO = System.IO;
+using Demo.Tools;
+
+namespace Company.Product.Module {
+  class Program {
+    class Nested {
+      public static int Seed() { return 7; }
+    }
+
+    public static int Main() {
+      const int N = 3;
+      var total = 0;
+      int[,] grid = new int[2,2];
+      int[] arr = new int[N];
+
+      int i = 0;
+      do {
+        arr[i] = i + Nested.Seed();
+        i = i + 1;
+      } while (i < N);
+
+      for (i = 0; i < N; i = i + 1) {
+        total = total + arr[i];
+      }
+
+      switch (total) {
+        case 24:
+          break;
+        default:
+          total = total > 0 ? total : 1;
+          break;
+      }
+
+      try {
+        return total;
+      } catch {
+        return 1;
+      }
+    }
+  }
+}
